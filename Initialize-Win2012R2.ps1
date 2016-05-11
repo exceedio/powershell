aebf4ab519c8073a6f383c950483ceed284cce6b
exceedio <jreese@exceedio.com>
2016-05-10 21:17:04 -0700

    Update Initialize-Win2012R2.ps1

--- Initialize-Win2012R2.ps1
+++ Initialize-Win2012R2.ps1
@@ -126,3 +126,4 @@ Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$f
 # install updates
 #
 Get-WUInstall -Criteria "IsInstalled = 0 AND BrowseOnly = 0 AND Type = 'Software'" -AutoReboot -AcceptAll
+
