82cfc3d66225bc1871fc915602ed9aca77d6a8e6
Jeff Reese <jreese@exceedio.com>
2016-05-26 12:11:54 -0700

    Added windows activation script

---
+++ Activate-Windows.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+    Activates Windows
+.DESCRIPTION
+    Uses WMI to enter the provided product key and activate Windows.
+.EXAMPLE
+    .\Activate-Windows.ps1 -ProductKey AAAAA-BBBBB-CCCCC-DDDDD-EEEEE
+.EXAMPLE
+    iwr https://raw.githubusercontent.com/exceedio/powershell/master/Activate-Windows.ps1 | iex
+#>
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string] $ProductKey,
+
+    [string] $ComputerName = $env:COMPUTERNAME
+)
+
+$service = gwmi -query "select * from SoftwareLicensingService" -ComputerName $ComputerName
+$service.InstallProductKey($ProductKey)
+$service.RefreshLicenseStatus()
