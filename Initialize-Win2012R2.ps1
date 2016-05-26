64cdc15bda3f0aeebe15e2807f7ef65a4104f34d
Jeff Reese <jreese@exceedio.com>
2016-05-26 00:01:32 -0700

    fixing invalid characters

--- Initialize-Win2012R2.ps1
+++ Initialize-Win2012R2.ps1
@@ -6,7 +6,7 @@
 .PARAMETER Name
     The name of the virtual machine
 .EXAMPLE
-    .\Initialize-Win2012R2.ps1 -Name VM1234 -Purpose 'Domain Controller' -IPv4 '192.168.1.200' -IPv4Gateway '192.168.1.1'
+    .\Initialize-Win2012R2.ps1 -Name VM1234 -Purpose 'Domain Controller'
 #>
 
 param(
@@ -14,23 +14,14 @@ param(
     [string] $Name,
 
     [Parameter(Mandatory=$true)]
-    [string] $Purpose,
-
-    [Parameter(Mandatory=$true)]
-    [string] $IPv4,
-
-    [Parameter(Mandatory=$true)]
-    [string] $IPv4Gateway,
-
-    [Parameter()]
-    [int] $IPv4PrefixLength = 24
+    [string] $Purpose
 )
 
 Function Install-NETFramework461
 {
     Write-Host "Installing .NET Framework 4.6.1..."
     $url = 'https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe'
-    $exe = Join-Path $PSScriptRoot 'NDP461-KB3102436-x86-x64-AllOS-ENU.exe'
+    $exe = "$env:temp\NDP461-KB3102436-x86-x64-AllOS-ENU.exe"
     $prm = "/q /norestart"
     Invoke-WebRequest $url -OutFile $exe
     & $exe $prm | Out-Host
@@ -41,7 +32,7 @@ Function Install-WMF5
 {
     Write-Host "Installing Windows Management Framework 5.0..."
     $url = 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu'
-    $msu = Join-Path $PSScriptRoot 'Win8.1AndW2K12R2-KB3134758-x64.msu'
+    $msu = "$env:temp\Win8.1AndW2K12R2-KB3134758-x64.msu"
     $prm = "$msu /quiet /forcerestart"
     Invoke-WebRequest $url -OutFile $msu
     & "wusa.exe" $prm | Out-Host
@@ -81,13 +72,6 @@ netsh interface ipv6 set global randomizeidentifiers=disabled store=active
 netsh interface ipv6 set global randomizeidentifiers=disabled store=persistent
 
 #
-# configure default network adapter
-#
-#$netadapter = Get-NetAdapter -Name 'Ethernet'
-#$netadapter | Set-NetIPInterface -Dhcp Disabled
-#$netadapter | New-NetIPAddress -IPAddress $IPv4 –PrefixLength $IPv4PrefixLength -DefaultGateway $IPv4Gateway
-
-#
 # enable smartscreen
 #
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer" -Name SmartScreenEnabled -Value 'RequireAdmin' -Force
@@ -95,7 +79,7 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer
 #
 # disable server manager from showing for current user
 #
-New-ItemProperty -Path "HKCU:\Software\Microsoft\ServerManager" -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value 1 –Force | Out-Null
+New-ItemProperty -Path "HKCU:\Software\Microsoft\ServerManager" -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value 1 -Force | Out-Null
 
 #
 # all users
@@ -126,4 +110,3 @@ Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$f
 # install updates
 #
 Get-WUInstall -Criteria "IsInstalled = 0 AND BrowseOnly = 0 AND Type = 'Software'" -AutoReboot -AcceptAll
-
