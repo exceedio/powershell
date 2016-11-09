2b8fca096480a4646b429b8b8dde4cb1cba67066
Jeff Reese <jreese@exceedio.com>
2016-11-09 12:22:50 -0800

    added reverse zone creation logic

--- New-ExceedioForest.ps1
+++ New-ExceedioForest.ps1
@@ -11,6 +11,16 @@
     Modified : Nov, 9, 2016
 #>
 
+function Convert-IPAddressToBinary ($dottedDecimal){ 
+  $dottedDecimal.split(".") | ForEach-Object {$binary=$binary + $([convert]::toString($_,2).padleft(8,"0"))} 
+  return $binary 
+} 
+ 
+function Convert-IPAddressToDottedDecimal ($binary){ 
+  do {$dottedDecimal += "." + [string]$([convert]::toInt32($binary.substring($i,8),2)); $i+=8 } while ($i -le 24) 
+  return $dottedDecimal.substring(1) 
+} 
+
 if (!((Get-WindowsFeature -Name AD-Domain-Services).Installed)) {
     Write-Warning "Installing ADDS"
     Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
@@ -26,7 +36,21 @@ if ((Get-WmiObject win32_computersystem).PartOfDomain -eq $false) {
     $testresult = Test-ADDSForestInstallation -DomainName $domainname -DomainNetbiosName $netbios -ForestMode Win2012R2 -DomainMode Win2012R2 -InstallDNS
     if ($testresult.Status -eq 'Success') {
         Install-ADDSForest -DomainName $domainname -DomainNetbiosName $netbios -ForestMode Win2012R2 -DomainMode Win2012R2 -InstallDNS
+        exit
     }
 } else {
-    Write-Output "Computer is already part of domain"
+    Write-Output "Computer is part of domain"
 }
+
+if (@(Get-DnsServerZone | Where-Object IsReverseLookupZone -eq $true).Length -le 3) {
+    Write-Warning "Creating reverse lookup zone"
+    $netaddress   = Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet
+    $ipv4address  = $netaddress.IPAddress
+    $prefixlength = $netaddress.PrefixLength
+    $network      = Convert-IPAddressToDottedDecimal $((Convert-IPAddressToBinary $ipv4address).substring(0,$prefixlength).padright(32,"0"))
+    $networkid    = "$network/$prefixlength"
+    Add-DnsServerPrimaryZone -NetworkID $networkid -ReplicationScope "Domain"
+} else {
+    Write-Output "Reverse lookup zone exists"
+}
+
