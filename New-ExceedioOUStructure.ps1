19fc680324ef87016b640768601db5c797b236e5
Jeff Reese <JReese@exceedio.com>
2016-12-06 15:56:10 -0800

    added new OU creation script

---
+++ New-ExceedioOUStructure.ps1
@@ -0,0 +1,24 @@
+﻿<#
+
+.SYNOPSIS
+    Creates a consistent OU structure.
+.DESCRIPTION
+    Use this script to create the initial OU structure in an active directory domain.
+.PARAMETER Name
+    The name of the virtual machine
+.EXAMPLE
+    iwr https://raw.githubusercontent.com/exceedio/powershell/master/New-ExceedioOUStructure.ps1 -UseBasicParsing | iex
+#>
+
+$base = Read-Host "Base OU (e.g. DC=contoso,DC=com)"
+
+New-ADOrganizationalUnit -Name 'Business' -Path "$base" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name 'Contacts' -Path "OU=Business,$base" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name 'Disabled Users' -Path "OU=Business,$base" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name 'Distribution Groups' -Path "OU=Business,$base" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name 'Security Groups' -Path "OU=Business,$base" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name 'Servers' -Path "OU=Business,$base" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name 'Users' -Path "OU=Business,$base" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name 'Workstations' -Path "OU=Business,$base" -ProtectedFromAccidentalDeletion $true
+
+Write-Output "Completed"
