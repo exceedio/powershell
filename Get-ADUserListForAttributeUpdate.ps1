7616bc3e17a65dc905113137f45ac4b280cdf216
Jeff Reese <JReese@exceedio.com>
2017-10-04 13:38:24 -0700

    Added script for gathering users for updating

---
+++ Get-ADUserListForAttributeUpdate.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    Gets a list of users and their attributes in a format that will be used to feed update.
+.DESCRIPTION
+    Run to generate a CSV file that can be handed to HR to be filled out with current,
+    accurate information. Ask them to return the file to you so that you can use the file
+    as input to an automated AD update.
+.EXAMPLE
+    iwr https://raw.githubusercontent.com/exceedio/powershell/master/Get-ADUserListForAttributeUpdate.ps1 | iex
+.EXAMPLE
+    iwr https://raw.githubusercontent.com/exceedio/powershell/master/Get-ADUserListForAttributeUpdate.ps1 -OutFile Get-ADUserListForAttributeUpdate.ps1
+    .\Get-ADUserListForAttributeUpdate.ps1 -SearchBase "OU=Users,OU=Business,DC=contoso,DC=com"
+.NOTES
+    Filename : Get-ADUserListForAttributeUpdate.ps1
+    Author   : jreese@exceedio.com
+    Modified : Oct 04, 2017
+#>
+
+param (
+    [string] $SearchBase
+)
+
+$date = (Get-Date).ToString('yyyyMMdd')
+$filename = $env:temp\userlistforadupdate-$date.csv
+Get-ADUser -Filter * -Properties * -SearchBase $SearchBase | ? {$_.UserPrincipalName -ne $null} | Sort UserPrincipalName | Select UserPrincipalName, GivenName, Surname, Title, Organization, Department, Office, EmailAddress, OfficePhone, MobilePhone, HomePhone, Fax, StreetAddress, City, State, PostalCode | Export-Csv $env:temp\Get-UsersForADUpdate.csv -NoTypeInformation
+Write-Output "File has been generated at $filename"
--- Get-O365LicenseAudit.ps1
+++ Get-O365LicenseAudit.ps1
@@ -3,27 +3,27 @@
     Gets Office 365 SKUs and user license assignments.
 .DESCRIPTION
     Generates two CSV files that can be used to determine how many licenses and of what
-	kind are purchased and assigned within a given organization. The script can be run
-	as a one-liner that deposits the CSV file in the current TEMP folder or the script
-	can be downloaded and run with arguments to direct the output to a folder of your
-	choice. Examples are provided below.
+    kind are purchased and assigned within a given organization. The script can be run
+    as a one-liner that deposits the CSV file in the current TEMP folder or the script
+    can be downloaded and run with arguments to direct the output to a folder of your
+    choice. Examples are provided below.
 
     This script requires the Azure AD V2 PowerShell module.
-	
-	Install-Module AzureAD
+    
+    Install-Module AzureAD
 
     Resource and background information on the commands used in this script are available
     at the website below.
-	
-	https://practical365.com/blog/managing-office-365-licenses-with-azure-ad-v2-powershell-module
+    
+    https://practical365.com/blog/managing-office-365-licenses-with-azure-ad-v2-powershell-module
 .EXAMPLE
     iwr https://raw.githubusercontent.com/exceedio/powershell/master/Get-O365LicenseAudit.ps1 | iex
 .EXAMPLE
     iwr https://raw.githubusercontent.com/exceedio/powershell/master/Get-O365LicenseAudit.ps1 -OutFile Get-O365LicenseAudit.ps1
-	.\Get-O365LicenseAudit.ps1 -OutPath C:\Temp
+    .\Get-O365LicenseAudit.ps1 -OutPath C:\Temp
 .EXAMPLE
     iwr https://raw.githubusercontent.com/exceedio/powershell/master/Get-O365LicenseAudit.ps1 -OutFile Get-O365LicenseAudit.ps1
-	.\Get-O365LicenseAudit.ps1 -Username someuser@contoso.com -OutPath C:\Temp
+    .\Get-O365LicenseAudit.ps1 -Username someuser@contoso.com -OutPath C:\Temp
 .NOTES
     Filename : Get-O365LicenseAudit.ps1
     Author   : jreese@exceedio.com
@@ -32,14 +32,14 @@
 
 param (
     [string] $Username,
-	[string] $OutPath = $env:temp
+    [string] $OutPath = $env:temp
 )
 
 function Export-SkuAudit
 {
     $skusfile  = "$OutPath\O365skus-$date.csv"
     Get-AzureADSubscribedSku | Select -Property Sku*,ConsumedUnits -ExpandProperty PrepaidUnits | Export-Csv -NoTypeInformation $skusfile
-	Write-Output "Office 365 SKU audit saved to $skusfile"
+    Write-Output "Office 365 SKU audit saved to $skusfile"
 }
 
 function Export-UserAudit
@@ -48,8 +48,8 @@ function Export-UserAudit
     $skulookup = @{}
     Get-AzureADSubscribedSku | % {$skulookup.Add($_.SkuId, $_.SkuPartNumber)}
     $users = Get-AzureADUser -Top 1000 | Where-Object {$_.AssignedLicenses.Length -gt 0} | Sort-Object DisplayName
-	$users | Select-Object DisplayName, Department, Mail, AccountEnabled, @{Expression={$skulookup[$_.AssignedLicenses[0].SkuId]};Label="Plan"} | Export-Csv -NoTypeInformation $usersfile
-	Write-Output "Office 365 User audit saved to $usersfile"
+    $users | Select-Object DisplayName, Department, Mail, AccountEnabled, @{Expression={$skulookup[$_.AssignedLicenses[0].SkuId]};Label="Plan"} | Export-Csv -NoTypeInformation $usersfile
+    Write-Output "Office 365 User audit saved to $usersfile"
 }
 
 $date = (Get-Date).ToString('yyyyMMdd')
