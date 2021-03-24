9052f6b7d6509434ac4f46693a75edfe4c5826d8
Jeff Reese <jreese@exceedio.com>
2021-03-24 14:47:13 -0700

    Added pauses in between steps

--- Initialize-HVServer2019.ps1
+++ Initialize-HVServer2019.ps1
@@ -92,6 +92,9 @@ function Enable-NICTeaming {
         if ($nics.Length -eq 4) {
             New-NetLbfoTeam -Name $vmteamname -TeamMembers NIC3,NIC4 -TeamNicName $vmswitchnic -TeamingMode SwitchIndependent -LoadBalancingAlgorithm Dynamic -Confirm:$false | Out-Null
         }
+        if ($nics.Length -eq 6) {
+            New-NetLbfoTeam -Name $vmteamname -TeamMembers NIC3,NIC4,NIC5,NIC6 -TeamNicName $vmswitchnic -TeamingMode SwitchIndependent -LoadBalancingAlgorithm Dynamic -Confirm:$false | Out-Null
+        }
         if ((Get-VMSwitch).Name -notcontains $vmswitchname) {
             if ($vmswitchnic -ne '') {
                 New-VMSwitch -Name $vmswitchname -NetAdapterName $vmswitchnic -AllowManagementOS 0 | Out-Null
@@ -138,7 +141,7 @@ function Install-OMSA {
         #
         if ($dellOmsaMspUrl) {
             Start-BitsTransfer -Source $dellOmsaMspUrl -Destination $dellOmsaMsp
-            Start-Process -FilePath "msiexec.exe" -ArgumentList @("/update","$dellOmsaMsp","/qb","/norestart")
+            Start-Process -FilePath "msiexec.exe" -ArgumentList @("/update","$dellOmsaMsp","/qb","/norestart") -Wait -NoNewWindow
         }
 
         #
@@ -160,16 +163,17 @@ function Install-OMSA {
 
 function Install-FiveNineManager {
     Invoke-WebRequest https://exdo.blob.core.windows.net/public/59Manager.msi -OutFile "$env:temp\59Manager.msi"
-    & msiexec.exe /i "$env:temp\59Manager.msi" /qb /norestart
+    Start-Process -FilePath "msiexec.exe" -ArgumentList @("/i","$env:temp\59Manager.msi","/qb","/norestart") -Wait -NoNewWindow
 }
 
 function Get-InstallMedia {
     if (!(Test-Path 'C:\Users\Public\Documents\ISO')) {
         New-Item -Path 'C:\Users\Public\Documents\ISO' -ItemType Directory -Force | Out-Null
     }
-    Invoke-WebRequest https://exdo.blob.core.windows.net/public/iso/SW_DVD9_Win_Server_STD_CORE_2016_64Bit_English_-4_DC_STD_MLF_X21-70526.ISO -OutFile 'C:\Users\Public\Documents\ISO\SW_DVD9_Win_Server_STD_CORE_2016_64Bit_English_-4_DC_STD_MLF_X21-70526.ISO'
-    Invoke-WebRequest https://exdo.blob.core.windows.net/public/iso/SW_DVD9_Win_Server_STD_CORE_2019_1809.1_64Bit_English_DC_STD_MLF_X22-02970.ISO -OutFile 'C:\Users\Public\Documents\ISO\SW_DVD9_Win_Server_STD_CORE_2019_1809.1_64Bit_English_DC_STD_MLF_X22-02970.ISO'
-    Invoke-WebRequest https://exdo.blob.core.windows.net/public/iso/SW_DVD9_Win_Pro_10_1903_64BIT_English_Pro_Ent_EDU_N_MLF_X22-02890.ISO -OutFile 'C:\Users\Public\Documents\ISO\SW_DVD9_Win_Pro_10_1903_64BIT_English_Pro_Ent_EDU_N_MLF_X22-02890.ISO'
+    Start-BitsTransfer -Source 'https://exdo.blob.core.windows.net/public/iso/SW_DVD9_Win_Server_STD_CORE_2019_1809.1_64Bit_English_DC_STD_MLF_X22-02970.ISO' -Destination 'C:\Users\Public\Documents\ISO\SW_DVD9_Win_Server_STD_CORE_2019_1809.1_64Bit_English_DC_STD_MLF_X22-02970.ISO'
+    #Invoke-WebRequest https://exdo.blob.core.windows.net/public/iso/SW_DVD9_Win_Server_STD_CORE_2016_64Bit_English_-4_DC_STD_MLF_X21-70526.ISO -OutFile 'C:\Users\Public\Documents\ISO\SW_DVD9_Win_Server_STD_CORE_2016_64Bit_English_-4_DC_STD_MLF_X21-70526.ISO'
+    #Invoke-WebRequest https://exdo.blob.core.windows.net/public/iso/SW_DVD9_Win_Server_STD_CORE_2019_1809.1_64Bit_English_DC_STD_MLF_X22-02970.ISO -OutFile 'C:\Users\Public\Documents\ISO\SW_DVD9_Win_Server_STD_CORE_2019_1809.1_64Bit_English_DC_STD_MLF_X22-02970.ISO'
+    #Invoke-WebRequest https://exdo.blob.core.windows.net/public/iso/SW_DVD9_Win_Pro_10_1903_64BIT_English_Pro_Ent_EDU_N_MLF_X22-02890.ISO -OutFile 'C:\Users\Public\Documents\ISO\SW_DVD9_Win_Pro_10_1903_64BIT_English_Pro_Ent_EDU_N_MLF_X22-02890.ISO'
 }
 
 function Set-ComputerName {
@@ -184,7 +188,8 @@ function Enable-iDRAC {
         $Address,
         $Netmask,
         $Gateway,
-        $Password
+        $Password,
+        $VlanId = 64
     )
     if (Test-Path "$env:programfiles\Dell\SysMgt\idrac\racadm.exe") {
         racadm set iDRAC.IPv4.Address $Address | Out-Null
@@ -192,28 +197,45 @@ function Enable-iDRAC {
         racadm set iDRAC.IPv4.Gateway $Gateway | Out-Null
         racadm set iDRAC.IPv4.DNS1 8.8.8.8 | Out-Null
         racadm set iDRAC.IPv4.DNS2 8.8.4.4 | Out-Null
+        racadm set iDRAC.Nic.VLanID $VlanId | Out-Null
+        racadm set iDRAC.Nic.VLanEnable 1 | Out-Null
         racadm set iDRAC.Users.2.Password $Password | Out-Null
     }
 }
 
-$Address = Read-Host "What is the desired iDRAC address (e.g. 192.168.0.201)?"
+$Address = Read-Host "What is the desired iDRAC address (e.g. 10.60.64.2)?"
 $Netmask = Read-Host "What is the desired iDRAC netmask (e.g. 255.255.255.0)?"
-$Gateway = Read-Host "What is the desired iDRAC gateway (e.g. 192.168.0.1)?"
+$Gateway = Read-Host "What is the desired iDRAC gateway (e.g. 10.60.64.1)?"
 $Password = Read-Host "What is the Dell iDRAC password?"
 
 Enable-SnmpService
+pause
 Disable-PrinterMapping
+pause
 Enable-RDP
+pause
 Disable-NetOffloadGlobalSettingTaskOffload
+pause
 Set-DvdRomDriveLetter
+pause
 Set-DataVolume
+pause
 Enable-NICTeaming
+pause
 Disable-VirtualMachineQueue
+pause
 Install-OMSA
+pause
 Enable-iDRAC -Address $Address -Netmask $Netmask -Gateway $Gateway -Password $Password
+pause
 Install-FiveNineManager
+pause
+Enable-TimeSynchronization
+pause
 Get-InstallMedia
+pause
 Enable-WindowsFirewall
+pause
 
 #
 # this last function restarts the computer
