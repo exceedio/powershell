7cd64deed4eadc79386721c9bbacfe65e38c9576
Jeff Reese <jreese@exceedio.com>
2021-04-06 14:06:39 -0700

    Replaced Start-BitsTransfer with Invoke-WebRequest

--- New-ExceedioWS.ps1
+++ New-ExceedioWS.ps1
@@ -21,11 +21,11 @@ param (
 Write-Output "Installing RSAT tools..."
 Get-WindowsCapability -Name Rsat* -Online | Where-Object State -ne Installed | Add-WindowsCapability -Online | Out-Null
 Write-Output "Installing 7-Zip..."
-Start-BitsTransfer -Source "https://www.7-zip.org/a/7z1900-x64.msi" -Destination "$env:TEMP\7z1900-x64.msi"
+Invoke-WebRequest -Uri "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile "$env:TEMP\7z1900-x64.msi"
 Start-Process "msiexec.exe" -ArgumentList @("/i", "$env:TEMP\7z1900-x64.msi", "/qb", "/norestart") -NoNewWindow -Wait
 Write-Output "Installing PuTTY..."
-Start-BitsTransfer -Source "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.74-installer.msi" -Destination "$env:TEMP\putty-64bit-0.74-installer.msi"
+Invoke-WebRequest -Uri "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.74-installer.msi" -OutFile "$env:TEMP\putty-64bit-0.74-installer.msi"
 Start-Process "msiexec.exe" -ArgumentList @("/i", "$env:TEMP\putty-64bit-0.74-installer.msi", "/qb", "/norestart") -NoNewWindow -Wait
 Write-Output "Installing git for Windows..."
-Start-BitsTransfer -Source "https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe" -Destination "$env:TEMP\Git-2.31.1-64-bit.exe"
+Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe" -OutFile "$env:TEMP\Git-2.31.1-64-bit.exe"
 Start-Process "$env:TEMP\Git-2.31.1-64-bit.exe" -ArgumentList @("/VERYSILENT") -Wait -NoNewWindow
